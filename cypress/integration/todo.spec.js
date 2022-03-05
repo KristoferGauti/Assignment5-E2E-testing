63e2d36e6f33440d9c1ed8ddcf68adc191e2e193
Kristofer Gauti <kristofer.thorhallsson@controlant.com>
2022-03-05 15:17:29 +0000

    Cypress tests done

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,3 +1,41 @@
+function getByTestId(testId) {
+    return cy.get(`[data-testid="${testId}"]`);
+}
+
+function clearAllTodos() {
+    getByTestId("todo-item").click({ multiple: true });
+}
+
 describe("📝 TODO app", () => {
-  // TODO Add your Cypress tests here
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+    it("TODO list is empty", () => {
+        getByTestId("todo-item").should("not.exist");
+    });
+    it("Add a new item to the list", () => {
+        getByTestId("todo-input").type("Test TODO item {enter}");
+        getByTestId("todo-item").should("exist");
+    });
+    it("Adds a second item to the list", () => {
+        clearAllTodos();
+        getByTestId("todo-input").type("Test TODO 1st item {enter}");
+        getByTestId("todo-input").type("Test TODO 2nd item {enter}");
+        getByTestId("todo-item").should("have.length", 2);
+    });
+    it("Removes one item from the list", () => {
+        clearAllTodos();
+        getByTestId("todo-input").type("Test TODO 1st item {enter}");
+        getByTestId("todo-input").type("Test TODO 2nd item {enter}");
+        getByTestId("todo-input").type("Test TODO 3rd item {enter}");
+        getByTestId("todo-item")
+            .then(($elems) => {
+                return Cypress.$.makeArray($elems);
+            })
+            .then((todosList) => {
+                const thirdTodoItem = todosList[todosList.length - 1];
+                thirdTodoItem.click();
+            });
+        getByTestId("todo-item").should("have.length", 2);
+    });
 });
