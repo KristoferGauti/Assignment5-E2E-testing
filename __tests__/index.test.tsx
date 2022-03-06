7cae1001d06db649f51f2401c6802cc0e211b2bb
Kristofer Gauti <kristofer.thorhallsson@controlant.com>
2022-03-06 00:55:28 +0000

    Jest component tests finished

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,11 +1,83 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import Home from "../pages/index";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 
-// Good starting point: https://testing-library.com/docs/react-testing-library/example-intro
+// Mock API is global
+const server = setupServer(
+    rest.get("/api/list", (req, res, ctx) => {
+        return res(
+            ctx.json([
+                {
+                    id: "Kristofer Gauti",
+                    title: "hello there",
+                },
+            ])
+        );
+    }),
+    rest.post("/api/add", (req, res, ctx) => {
+        return res(ctx.json({ id: "new todo", title: "homie is sleepy" }));
+    }),
+    rest.delete("/api/remove", (req, res, ctx) => {
+        return res(ctx.json({ id: "new todo", title: "homie is sleepy" }));
+    })
+);
 
-// TODO setup your mock api here
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-describe("Tests", () => {
-  // TODO Add your react-testing-library tests here
+describe("Component tests", () => {
+    test("Loading component is displayed when the response is not correct", async () => {
+        server.use(
+            rest.get("/api/list", (req, res, ctx) => {
+                return res(ctx.json(null));
+            })
+        );
+        render(<Home />);
+        await waitFor(() => {
+            return expect(screen.getByTestId("loading")).toBeInTheDocument();
+        });
+    });
+    test("A single item is in the list when the <Home /> component is loaded", async () => {
+        render(<Home />);
+        await waitFor(() => {
+            return expect(screen.getByTestId("todo-item")).toBeInTheDocument();
+        });
+    });
+    test("Adds a new item to the list", async () => {
+        render(<Home />);
+        await waitFor(() => {
+            return expect(screen.getByTestId("todo-item")).toBeInTheDocument();
+        });
+        await waitFor(async () => {
+            const todoInput = await screen.findByTestId("todo-input");
+            fireEvent.change(todoInput, {
+                target: {
+                    value: "Practice the violin",
+                },
+            });
+            fireEvent.submit(todoInput);
+        });
+        await waitFor(() => {
+            return expect(screen.getAllByTestId("todo-item")).toHaveLength(2);
+        });
+    });
+    test("Removes an item from the list", async () => {
+        render(<Home />);
+        await waitFor(() => {
+            return expect(screen.getByTestId("todo-item")).toBeInTheDocument();
+        });
+        await waitFor(async () => {
+            const todoItems = await screen.findAllByTestId("todo-item"); // findAllByTestId is async function
+            for (const todoItem of todoItems) {
+                fireEvent.click(todoItem);
+            }
+        });
+        await waitFor(() => {
+            return expect(screen.queryAllByTestId("todo-item")).toHaveLength(0);
+        });
+    });
 });
