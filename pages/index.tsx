d3a5c2f697bde4aec3f6a675048febcd764eeaef
Kristofer <kristofergauti@gmail.com>
2022-03-14 01:24:03 +0000

    testing if the workflow for automating deployment works

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
             <main className={styles.main}>
                 <div className={styles.grid}>
                     <h1 className={styles.title}>
-                        TODO 💪🦾 App with{" "}
+                        TODO 🦾 App with{" "}
                         <a href="https://blog.upstash.com/nextjs-todo">
                             Next.js!
                         </a>
