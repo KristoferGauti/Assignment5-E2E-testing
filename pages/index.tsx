bbf7083a1c1a6efc14fa591aeeb5f6551c4730f0
Kristofer <kristofergauti@gmail.com>
2022-03-14 11:57:53 +0000

    Please work my homie

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ export default function Home() {
             <main className={styles.main}>
                 <div className={styles.grid}>
                     <h1 className={styles.title}>
-                        TODO 🦾😱 App with{" "}
+                        TODO 🦾 App with{" "}
                         <a href="https://blog.upstash.com/nextjs-todo">
                             Next.js!
                         </a>
